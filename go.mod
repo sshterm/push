a72d9acb4a185fd3bf59390af879416fcd5d036e
ssh term <admin@ssh2.app>
2025-02-18 17:00:35 +0800

    dev

--- go.mod
+++ go.mod
@@ -3,20 +3,34 @@ module github.com/sshterm/push
 go 1.23.0
 
 require (
+	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/labstack/echo/v4 v4.13.3
+	github.com/shirou/gopsutil/v4 v4.25.1
 	github.com/sideshow/apns2 v0.25.0
 	golang.org/x/time v0.8.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/ebitengine/purego v0.8.2 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
--- server.go
+++ server.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	_ "embed"
-	"encoding/json"
-	"io"
 	"log"
 	"net/http"
 	"strings"
 	"time"
 
+	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/sideshow/apns2"
@@ -40,6 +39,7 @@ func main() {
 	clientDev := apns2.NewTokenClient(token).Development()
 	client := apns2.NewTokenClient(token).Production()
 	e := echo.New()
+	e.Validator = &CustomValidator{validator: validator.New()}
 	e.Use(middleware.BodyLimit("64K"))
 	config := middleware.RateLimiterConfig{
 		Skipper: middleware.DefaultSkipper,
@@ -57,20 +57,14 @@ func main() {
 			return context.JSON(http.StatusTooManyRequests, nil)
 		},
 	}
-
 	e.Use(middleware.RateLimiterWithConfig(config))
 	e.POST("/apn_push", func(c echo.Context) error {
-		body, err := io.ReadAll(c.Request().Body)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "Please send a request body")
+		data := new(Body)
+		if err = c.Bind(data); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 		}
-		var data Body
-		err = json.Unmarshal(body, &data)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "Invalid request body")
-		}
-		if data.Token == "" || data.Topic == "" || data.Notification.APS.Alert.Title == "" || data.Notification.APS.Alert.Subtitle == "" {
-			return echo.NewHTTPError(http.StatusBadRequest, "Invalid request body")
+		if err = c.Validate(data); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 		}
 		var valid bool
 		for _, v := range topic {
@@ -110,22 +104,35 @@ func main() {
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-type Body struct {
-	Dev          bool         `json:"dev"`
-	Token        string       `json:"token"`
-	Topic        string       `json:"topic"`
-	Notification Notification `json:"notification"`
-	Priority     int          `json:"priority"`
+type CustomValidator struct {
+	validator *validator.Validate
 }
-type APS struct {
-	Alert Alert  `json:"alert"`
-	Sound string `json:"sound"`
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
 }
-type Alert struct {
-	Title    string `json:"title"`
-	Subtitle string `json:"subtitle"`
-	Body     string `json:"body"`
-}
-type Notification struct {
-	APS APS `json:"aps"`
-}
+
+type (
+	Body struct {
+		Dev          bool         `json:"dev"`
+		Token        string       `json:"token" validate:"required"`
+		Topic        string       `json:"topic" validate:"required"`
+		Notification Notification `json:"notification" validate:"required"`
+		Priority     int          `json:"priority" validate:"required"`
+	}
+	APS struct {
+		Alert Alert  `json:"alert" validate:"required"`
+		Sound string `json:"sound"`
+	}
+	Alert struct {
+		Title    string `json:"title" validate:"required"`
+		Subtitle string `json:"subtitle" validate:"required"`
+		Body     string `json:"body" validate:"required"`
+	}
+	Notification struct {
+		APS APS `json:"aps" validate:"required"`
+	}
+)
